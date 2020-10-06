109a6e6090845b8f102ab0d022bf8c5575277ddd
Reuven Peleg <reuvenpe2005+code@gmail.com>
2020-10-06 20:26:46 +0300

    Small UI touchups

--- follow_hwc_frontend/src/App.js
+++ follow_hwc_frontend/src/App.js
@@ -6,6 +6,8 @@ import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav'
 import Card from 'react-bootstrap/Card'
 import Container from 'react-bootstrap/Container'
+import Accordion from 'react-bootstrap/Accordion'
+
 
 function Game(props) {
   return <p>
@@ -33,10 +35,11 @@ function Match(props) {
   const [result1, result2] = details.result || [null, null];
   const {games} = details;
 
-  return <Card className="mx-auto my-2">
-    <Card.Body>
-    <Card.Title>{opponent1} vs {opponent2} ({result1} - {result2})</Card.Title>
-    <Card.Subtitle>Took place on {details.date}</Card.Subtitle>
+  return <Card className="mx-auto my-1">
+    <Card.Body>     
+      <Card.Title>{opponent1} vs {opponent2}</Card.Title>
+      <Card.Subtitle>Took place on {details.date}, result {result1} - {result2}</Card.Subtitle>
+
     {
       games.length ? 
         <Games ids={games}/> : 
@@ -49,10 +52,16 @@ function Match(props) {
 function Round(props) {
   const {number, matches} = props;
 
-  return <>
-    <p>Round number {number}</p>
-    {matches?.map(m => <Match details={m}></Match>)}
-  </>
+  return <div>
+    <Accordion.Toggle as={Button} variant="link" eventKey={number}>
+      Round number {number}
+    </Accordion.Toggle>
+    <Accordion.Collapse eventKey={number}>
+      <div>
+        {matches?.map(m => <Match details={m}></Match>)}
+      </div>
+    </Accordion.Collapse>
+  </div>
 }
 
 class Matches extends React.Component {
@@ -90,7 +99,9 @@ class Matches extends React.Component {
     <h1>Matches:</h1>
     {isLoaded || <p>Loading...</p>}
     {error && <p>Error: {error?.toString()}</p>}
-    {rounds?.rounds?.map(r => <Round {...r}/>)}
+    <Accordion>
+      {rounds?.rounds?.map(r => <Round {...r}/>)}
+    </Accordion>
     </>;
   }
 }
