2b8fd55941a2ec29d49e62a3a154d4e944936a43
Reuven Peleg <reuvenpe2005+code@gmail.com>
2020-10-06 12:06:59 +0300

    Make a script for getting games data from match forum message

--- follow_hwc_frontend/src/App.js
+++ follow_hwc_frontend/src/App.js
@@ -1,26 +1,75 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+function Game(props) {
+  //     <a href={`https://lichess.org/${props.id}`} target="_blank">Link</a>
+  return <p>
+    <iframe
+      src={`https://lichess.org/embed/${props.id}#1000?theme=auto&amp;bg=auto`}
+      width={600} height={397} frameborder={0}></iframe>
+  </p>
+}
+
+function Match(props) {
+  const {details} = props;
+  const [opponent1, opponent2] = details.opponents;
+  const {games} = details;
+
+  return <>
+    <h1>{opponent1} vs {opponent2}</h1>
+    <p>Took place on {details.date}</p>
+    {games.length ? games.map(g =><Game id={g}/>) : <p>No games played</p>}
+  </>;
+}
+
+class Matches extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      matches: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("matches.json")
+      .then(res => res.json())
+      .then(
+        result => {
+          this.setState({
+            isLoaded: true,
+            matches: result
+          })
+        },
+        error => {
+          this.setState({
+            isLoaded: true,
+            error
+          })
+        });
+  }
+
+  render() {
+    const {isLoaded, matches, error} = this.state;
+    return <>
+    <h1>Matches:</h1>
+    {isLoaded || <p>Loading...</p>}
+    {error && <p>Error: {error?.toString()}</p>}
+    <p>{matches?.map(m => <Match details={m}/>)}</p>
+    </>;
+  }
+}
 
 function App() {
   return (
-    <h>Follow </h>
+    <>
+    <h1>Follow Horde World Championship</h1>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Matches/>
     </div>
+    </>
   );
 }
 
---
+++ parse_match_time.py
@@ -0,0 +1,73 @@
+#! /usr/bin/python3
+"""
+Parse matches times from the Lichess thread
+"""
+import sys
+import json
+import parsedatetime
+from urllib.parse import urlencode
+import requests
+try: 
+    from BeautifulSoup import BeautifulSoup
+except ImportError:
+    from bs4 import BeautifulSoup
+from datetime import datetime, date, timedelta
+calandar = parsedatetime.Calendar()
+source_date = datetime(2020, 9, 1, 0, 0, 0)
+
+
+def get_games(opponent1, opponent2, start_date, end_date):
+    search_params = {
+        "players.a": opponent1,
+        "players.b": opponent2,
+        #"perf": 50,
+        # TC in the tournoment is 2+2
+        "clock.initMin": 120,
+        "clock.initMax": 120,
+        "clock.incMin": 2,
+        "clock.incMax": 2,
+        "dateMin": str(start_date),
+        "dateMax": str(end_date),
+        "sort.field": "d",
+        "sort.order": "asc"
+    }
+    search_url = "https://lichess.org/games/search?" + urlencode(search_params)
+    print(search_url)
+    response = requests.get(search_url)
+    parsed_response = BeautifulSoup(response.content, features='html.parser')
+    game_ids = []
+    for game_row in parsed_response.find_all('article', class_='game-row'):
+        game_link_element = game_row.find_all('a', class_='game-row__overlay')[0]
+        game_id = game_link_element['href'].split('/')[1]
+        game_ids.append(game_id)
+    return game_ids
+
+
+def parse_match_line(line):
+    words = line.split(' ')
+    return words[0].strip('@'), words[2].strip('@'), calandar.parse(' '.join(words[3:]), sourceTime=source_date)
+
+
+def main():
+    if len(sys.argv) != 2:
+        print(f"Usage: {sys.argv[0]} MATCH_LINE")
+        return
+    opponent1, opponent2, parsed_date = parse_match_line(sys.argv[1])
+    time_struct, status = parsed_date
+    if status == 3:
+        match_time = datetime(*time_struct[:6])
+        games = get_games(opponent1, opponent2, match_time.date() - timedelta(days=1), match_time.date() + timedelta(days=1))
+    else:
+        print("Failed to parse datetime")
+        match_time = ""
+        games = []
+
+    print(json.dumps({
+        "opponents": [opponent1, opponent2],
+        "date": str(match_time),
+        "games": games
+    }, indent=1))
+
+
+if __name__ == "__main__":
+    main()
